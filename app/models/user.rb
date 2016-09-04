02762cfa8339db52feb415711ba4af2e55c7f678
Pipsik <531621@mail.ru>
2016-09-04 20:58:30 +0300

    lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,28 @@ class User < ActiveRecord::Base
 
   def self.find_for_facebook_oauth access_token
     user = User.where(:email => access_token.info.email).first_or_create do |user|
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.extra.raw_info.name, :nickname => access_token.extra.raw_info.username, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20]) 
+       user.provider=access_token.provider
+       user.username=access_token.extra.raw_info.name
+       user.email=access_token.extra.raw_info.email
+       user.password=Devise.friendly_token[0,20]
     end
   end
 
   def self.find_for_vkontakte_oauth access_token
     user = User.where(:email => access_token.info.email).first_or_create do |user|
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.info.email, :password => Devise.friendly_token[0,20]) 
+       user.provider=access_token.provider
+       user.username=access_token.info.name
+       user.email=access_token.info.email
+       user.password=Devise.friendly_token[0,20]
     end
   end
 
   def self.find_for_twitter_oauth access_token
     user = User.where(:email => access_token.info.email).first_or_create do |user|
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.info.email, :password => Devise.friendly_token[0,20]) 
+       user.provider=access_token.provider
+       user.username=access_token.info.name
+       user.email=access_token.info.email
+       user.password=Devise.friendly_token[0,20]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",:registrations => "users/registrations" }
   resources :users, :only => [:index, :destroy]
   root :to => 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
