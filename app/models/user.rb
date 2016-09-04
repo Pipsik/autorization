18adc1bf85780b85454b6dd6b95e89f69989d790
Pipsik <531621@mail.ru>
2016-09-04 17:58:39 +0300

    fse

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_facebook_oauth access_token
+    if user = User.where(:email => access_token.info.email).first_or_create
+      user
+    else 
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.extra.raw_info.name, :nickname => access_token.extra.raw_info.username, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
+
+  def self.find_for_vkontakte_oauth access_token
+    if user = User.where(:email => access_token.info.email).first_or_create
+      user
+    else 
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.info.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
+
+  def self.find_for_twitter_oauth access_token
+    if user = User.where(:email => access_token.info.email).first_or_create
+      user
+    else 
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.info.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
+end
