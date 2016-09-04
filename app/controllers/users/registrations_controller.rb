fffd4b50c9d0c49d815056097fc78e4ddd407b02
Pipsik <531621@mail.ru>
2016-09-04 22:56:34 +0300

    fsfs

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation,:provider)
   end
 end
